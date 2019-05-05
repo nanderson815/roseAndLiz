2f0fe12b03ef356a35ee776f02d30d9eea309ec0
Noah A <nanderson815@gmail.com>
2019-05-04 21:47:22 -0400

    styling home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './Home.css';
+import Button from '@material-ui/core/Button';
 
 
 const Home = (props) => {
     return (
         <div>
             <header className="header">
-
+                <Button className="" size="large">Shop Now</Button>
             </header>
         </div>
     )
